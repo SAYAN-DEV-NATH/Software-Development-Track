4bc92c5a7fb4c54becb9e62968911169bb955daf
SAYAN-DEV-NATH <sayandevnath01@gmail.com>
2024-10-03 00:31:44 +0600

    update

---
+++ Python/F_conditional.py
@@ -0,0 +1,31 @@
+# in, not, not in, is, is not
+
+a = 7
+if (a > 5):
+    print("5 is besi")
+    print("Koto besi ke jane")
+elif a > 3:
+    print("olpo boro")
+elif a == 2:
+    print("ekbom dui er soman soman")
+else:
+    print("choto choto raate lambi hoye")
+
+
+boss = True
+
+if  boss:
+    print("tel er bakso niye astesi boss re tell dibo")
+else:
+    print("lunch er pore asen")
+
+
+coin = "Head"
+if boss == True or boss != False:
+    print("boss you are joss")
+    if coin == "Head":
+        print("bating")
+    else:
+        print("bowling")
+else:
+    print("you are loss")
