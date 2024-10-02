263322bf67883cb316a2d221073418ceea2bd3e8
SAYAN-DEV-NATH <sayandevnath01@gmail.com>
2024-10-02 11:00:36 +0600

    update

---
+++ Python/variabe.py
@@ -0,0 +1,25 @@
+age = 45
+interest_rate = 2.5
+name = "Jume e jo Pathan"
+district = 'B Baria'
+is_single = True
+is_sleeping = False
+
+print(age)
+print(interest_rate)
+print(name)
+print(district)
+print(is_single)
+print(is_sleeping)
+print(age+interest_rate)
+print(type(age))
+
+# single line comment (ctrl + '/')
+"""
+multiple line comment (ctrl + shift + a) 
+"""
+
+print("Kodom Ali" + "Kacha badam")
+text = f"Kodom ali {age} living in {district}. Interesting with rate {interest_rate}%"
+print(text)
+print("Kodom ali ",age," living in ",district,". Interesting with rate ",interest_rate,"%")
