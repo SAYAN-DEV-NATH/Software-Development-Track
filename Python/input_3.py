8e34e574c4a90a49d3b71bea59dc9b567f5ffad6
SAYAN-DEV-NATH <sayandevnath01@gmail.com>
2024-10-02 11:24:03 +0600

    update

---
+++ Python/input_3.py
@@ -0,0 +1,15 @@
+# print('Now I need money')
+# money = input('Now I need money: ')
+# print(money)
+# print('Here is your money:',money )
+
+# first_money = input('Kodom ali, Dosto kichu taka dey: ')
+# second_money = input('Peyara begum, Dosto kichu taka dey: ')
+# print(first_money, second_money)
+
+# By default the input from user will be string type:
+# print(first_money+second_money)
+
+first_money = int(input('Kodom ali, Dosto kichu taka dey: '))
+second_money = int(input('Peyara begum, Dosto kichu taka dey: '))
+print(first_money+second_money)
