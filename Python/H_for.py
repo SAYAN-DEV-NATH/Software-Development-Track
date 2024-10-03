d453624e0bc1a8c6fe9baa4981b87e8cba4b28c6
SAYAN-DEV-NATH <sayandevnath01@gmail.com>
2024-10-03 08:18:18 +0600

    update

--- Python/H_for.py
+++ Python/H_for.py
@@ -1,8 +1,29 @@
-numbers = [5, 10, 15, 20, 25]
-for nums in numbers:
-    print(nums)
+# numbers = [5, 10, 15, 20, 25]
+# for nums in numbers:
+#     print(nums)
 
-sum = 0
-for nums in numbers:
-    sum += nums
-print(sum)
+# sum = 0
+# for nums in numbers:
+#     sum += nums
+# print(sum)
+
+# text = "sayan dev nath"
+# for char in text:
+#     print(char)
+
+
+# print(range(1, 10))
+# for value in range(1, 10):
+#     print(value, end=" ")
+
+# for index, value in enumerate(range(1, 10)):
+#     # print(f"Index: {index}, Value: {value}")
+#     # print("index:", index, " value:", value) # autometic space have
+#     print(f"Index: {index}, Value: {value}")  # automatic space nai
+
+friends = ["nobel", "ashir", "rabi", "naz"]
+# for friend in friends:
+#     print(f"Friend: {friend}", end=" ")
+
+for index, value in enumerate(friends):
+    print(index, value)
