45c1b93b8b87261602da5141016178c4ce12f463
SAYAN-DEV-NATH <sayandevnath01@gmail.com>
2024-10-08 15:30:50 +0600

    update

---
+++ Python/Week - 1/Module - 03/A_string.py
@@ -0,0 +1,28 @@
+name = "Sakib Khan"  # escape (\')
+# name2 = "sakib Khan"
+# name3 = """
+# Sakib Khan
+# Number One
+# """
+# # print(name)
+
+# for char in name:
+#     print(char, end=" ")
+# print(name[3])
+# print(name[:5])
+# print(name[1:5])
+
+# print(name[::-1])  # reverse string
+
+# mutable means changeable
+# immutable means you can't change it
+
+# name[0] = "R"  # not working in python
+# print(name)
+
+if "Khan" in name:
+    print("Yes")
+else:
+    print("No")
+
+print(name.upper())
---
+++ Python/Week - 1/Module - 03/B_tuple.py
@@ -0,0 +1,12 @@
+def multiple():
+    return 3, 4
+
+
+print(multiple())  # tuple
+print(type(multiple()))
+
+things1 = (1, 2, 3, 4, 5)  # value does not change able
+print(type(things1))
+
+things2 = [1, 2, 3, 4, 5]
+print(type(things2))
